4adba559216f2150ec73faf085e6177cad69dcae
natruszka <ola.michalska2002@gmail.com>
2024-05-19 22:17:03 +0200

    Revert "change folder structure"
    This reverts commit 123bcb67704449c0a16df1bfd59426d0ea06a049.

---
+++ WeatherApp/Database/Entities/WeatherData.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherApp.Database.Entities;
+
+public class WeatherData
+{
+    public int DataId { get; set; }
+    public int LocationId { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+    public decimal Temperature { get; set; }
+    public decimal MinTemperature { get; set; }
+    public decimal MaxTemperature { get; set; }
+    public decimal Humidity { get; set; }
+    public decimal WindSpeed { get; set; }
+    public decimal Precipitation { get; set; }
+}

123bcb67704449c0a16df1bfd59426d0ea06a049
natruszka <ola.michalska2002@gmail.com>
2024-05-19 22:13:04 +0200

    change folder structure

--- WeatherApp/Resources/ErrorMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeatherApp.Resources;
-
-public static class ErrorMessages
-{
-    public static string ServerNotFound = "Nie odnaleziono serwera.";
-    public static string TransactionFailure = "Transakcja się nie powiodła.";
-    public static string RadiusOutOfRange = "Promień nie może być ujemny";
-}

de3ef7094152e52e6e20ba99799e2eeffe53716e
natruszka <ola.michalska2002@gmail.com>
2024-05-26 15:33:50 +0200

    delete duplicate files

--- API/WeatherApp/Services/Interfaces/ILocationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using WeatherApp.Database.Entities;
-
-namespace WeatherApp.Services.Interfaces;
-
-public interface ILocationService
-{
-    Task<Dictionary<string, List<Location>>> GetAllLocations();
-    Task<List<Location>> GetLocationFromServer(int serverNum);
-}
